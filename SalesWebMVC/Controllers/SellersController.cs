9912551737462e2f16e1607494bef3b2c5a19d5d
jeanmartins <jean.carlos.martins00@gmail.com>
2022-02-22 18:05:42 -0300

    Validation

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -25,6 +25,7 @@ namespace SalesWebMVC.Controllers {
             var list = _sellerService.FindAll();
             return View(list);
         }
+
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
@@ -35,6 +36,16 @@ namespace SalesWebMVC.Controllers {
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel
+                {
+                    Seller = seller,
+                    Departments = departments
+                };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -98,6 +109,16 @@ namespace SalesWebMVC.Controllers {
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel
+                {
+                    Seller = seller,
+                    Departments = departments
+                };
+                return View(viewModel);
+            }
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
